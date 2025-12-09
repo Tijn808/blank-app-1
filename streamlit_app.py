6c6a70c18fafc1d97c4ec57cf6f783bfcafa361d
Tijn808 <T.saes@student.maastrichtuniversity.nl>
2025-12-09 11:09:40 +0000

    version1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# research question
+# column selection
+
+st.markdown('## Train-Test Split')
+
+with st.expander ('# Train-Test Split'):
+    st.info('We .....')
+
+st.markdown('## Identifying Problems in Basic Data')
+with st.expander ('# Missing Values'):
+    st.info ('Handling missing values')
+
+with st.expander ('# -')
